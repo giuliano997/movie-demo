a695728453bdd63fed22752e92fb66c36e1a4d64
Giuliano <giuliano.schweiger97@web.de>
2024-04-10 16:08:33 +0200

    basic styling moviecard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,8 @@
+import "./Moviecard.css";
 export function MovieCard (title) {
     console.log(title);
     return(
-        <li> 
+        <li className="movie-card" >
         <h2>{title}</h2>
       </li>
     
